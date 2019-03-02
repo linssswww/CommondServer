6bd628cfe95677ebaf49bebc54b001a7a7e70a1f
Admin <Admin@PS-1>
2019-03-03 02:02:40 +0800

    添加项目文件。

---
+++ CommondServer/Http.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+namespace myHttp
+{
+	class Http
+	{
+	public:
+		Http();
+		virtual ~Http();
+		void BuildHttpServer();
+		void GetRequest();
+		void PostRequest();
+	};
+	class ParseHttpProTocol
+	{
+	public:
+		ParseHttpProTocol();
+		virtual ~ParseHttpProTocol();
+		
+	private:
+		int content_length;
+		std::string content;
+	};
+	class WinSocket
+	{
+	public:
+		WinSocket();
+		virtual ~WinSocket();
+		void BuildWinSocket();
+		void CloseWinSocket();
+		void Recv();
+		void Send();
+	};
+	class TcpServer:public WinSocket
+	{
+	public:
+		TcpServer();
+		virtual ~TcpServer();
+		void listen();
+		void recv();
+		void send();
+	};
+}
