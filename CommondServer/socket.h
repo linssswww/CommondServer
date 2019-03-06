ab1c3071ed4291f08820e0bef1dab91b303b4e3b
Admin <Admin@PS-1>
2019-03-06 23:59:33 +0800

    1、解释了select 原理

--- CommondServer/socket.h
+++ CommondServer/socket.h
@@ -24,7 +24,13 @@ namespace mySocket
 		void HandleSocketSet();
 		void Select();
 		
-
+		/*
+		select 整体使用思路
+		1、select 判断是否有触发 fd_num =0 没有信号。 如果》0 有信号
+		2、知道有信号，但不知道是哪一个信号，所以通过轮询的办法判断是新连接还是数据过来
+		3、select 当无任何的操作的时候， 集合应该是被清空的， 当有新连接或者数据过来， 集合中才有东西
+		
+		*/
 	private:
 		WSADATA wsadata;
 		SOCKET ServSock;
